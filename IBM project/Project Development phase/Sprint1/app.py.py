0edcb269db1e43d42bb87df87575e4e373ff8cf9
safee60 <112847635+safee60@users.noreply.github.com>
2022-11-09 20:04:47 +0530

    Add files via upload

---
+++ IBM project/Project Development phase/Sprint1/app.py.py
@@ -0,0 +1,144 @@
+from flask import Flask,render_template,request,redirect,url_for ,session
+import ibm_db
+import re
+import os
+import math
+import random
+import smtplib
+app=Flask(__name__,template_folder='templates',static_folder='static')
+app.secret_key='a'
+conn = ibm_db.connect("Database=bludb;Hostname=1bbf73c5-d84a-4bb0-85b9-ab1a4348f4a4.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud;Port=32286;Security=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=gqn68734;PWD=IJvrQIkrmldUdQzP",'','')
+print("successfully connected")
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():#forget_pwd
+    global userid
+    msg=''
+    print(21)
+    
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        sql='SELECT * FROM USER WHERE username=? AND password=?'#oldpwd=?
+        stmt=ibm_db.prepare(conn,sql)
+        ibm_db.bind_param(stmt,1,username)
+        ibm_db.bind_param(stmt,2,password)
+        ibm_db.execute(stmt)
+        account=ibm_db.fetch_assoc(stmt)
+        print(account)
+        if account:
+            session['Logged in']=True
+            session['id']=account['USERNAME']
+            userid=account['USERNAME']
+            session['username']=account['USERNAME']
+            msg='Logged in successfully'
+            return render_template('login.html',msg=msg)
+        else:
+            msg='Incorrect username/password'
+    return render_template('login.html',msg=msg)
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    msg=''
+    if request.method =='POST':
+        username=request.form['username']
+        email=request.form['email']
+        password=request.form['password']
+        Firstname=request.form['firstname']
+        lastname=request.form['lastname']
+        #phoneno=request.form['phoneno']
+        sql='SELECT * FROM USER WHERE username=?'
+        stmt=ibm_db.prepare(conn,sql)
+        ibm_db.bind_param(stmt,1,username)
+        #ibm_db.bind_param(stmt,2,password)
+        ibm_db.execute(stmt)
+        account=ibm_db.fetch_assoc(stmt)
+        print(account)
+        if account:
+            msg="Account already exist!"
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+',email):
+            msg="Invalid email address"
+        elif not re.match(r'[A-Za-z0-9]+',username):
+            msg="name must contain character and numbers"
+       
+        else:
+            insert_sql='INSERT INTO USER values(?,?,?,?,?)'
+            prep_stmt=ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(prep_stmt,1,username)
+            ibm_db.bind_param(prep_stmt,2,email)
+            ibm_db.bind_param(prep_stmt,3,password)
+            ibm_db.bind_param(prep_stmt,4,Firstname)
+            ibm_db.bind_param(prep_stmt,5,lastname)
+            ibm_db.execute(prep_stmt)
+            msg="You have successfully registered"
+            return render_template('verify.html',msg=msg)
+    elif request.method=="POST":
+        msg="Please fill out the form"
+    return render_template('register.html',msg=msg)
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/verify')
+def verify():
+        email=request.args.get('email', None)
+        server=smtplib.SMTP('smtp.gmail.com',587)
+        server.starttls()
+        password="nsgeuedwbzptosyp"
+        server.login(email,password)
+        otp=''.join([str(random.randint(0,9))for i in range(4)])
+        msg=' YOUR OTP IS'+str(otp)
+        server.sendmail(email,email,msg)
+        server.quit()
+        if request.method=='POST':
+            verify=request.method['code']
+            if verify==otp:
+                return render_template('login.html')
+        return render_template('verify.html')
+    
+@app.route('/frgpwd', methods=['GET','POST'])
+def forgotpassword():
+    msg =" "
+    print(request.form)
+    username1=request.form['username']
+    oldpassword=request.form['oldpassword']
+    newpassword=request.form['newpassword']
+    sql='SELECT * FROM USER WHERE username=?'
+    stmt=ibm_db.prepare(conn,sql)
+    ibm_db.bind_param(stmt,1,username1)
+    #ibm_db.bind_param(stmt,2,password)
+    ibm_db.execute(stmt)
+    account=ibm_db.fetch_assoc(stmt)
+    print(account)
+    if account:
+        chgpwd_sql='UPDATE USER SET password = ? WHERE username = ?'
+        prep_stmt=ibm_db.prepare(conn, chgpwd_sql)
+        ibm_db.bind_param(prep_stmt,1,newpassword)
+        ibm_db.bind_param(prep_stmt,2,username1)
+        ibm_db.execute(prep_stmt)
+        msg="You have successfully changed password"
+        return render_template('forgot password.html',msg=msg)
+    return render_template('forgot password.html',msg=msg)
+
+
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin',None)
+    session.pop('id',None)
+    session('username',None)
+    return render_template("home.html") 
+
+if __name__=="__main__":
+    app.run(debug=True ,host='0.0.0.0',use_reloader=False)
+     
+     
+           
+        
+        
+        
